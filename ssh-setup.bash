cefd4ed0a639626b7ae193e7b80be22c27be194f
Felix <felix.kirchhoff@neuefische.de>
2024-01-26 12:05:42 +0100

    add bash script for windows setup

---
+++ ssh-setup.bash
@@ -0,0 +1,12 @@
+read -p "Enter your full name (e.g. Jane Doe): " name &&
+read -p "Enter your email address you use on GitHub: " email &&
+git config --global user.name $name
+git config --global user.email $email
+# ssh-keygen -t ed25519 -C $email &&
+eval "$(ssh-agent -s)" &&
+ssh-add ~/.ssh/id_ed25519 &&
+echo
+echo Success!
+echo "SSH Keys generated. Please open https://github.com/settings/keys and add the following public key:"
+echo
+cat ~/.ssh/id_ed25519.pub
